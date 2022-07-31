d65c8de92a37b0bb883c79068ff357f5790e1297
= <=dave.jodatek@gmail.com>
2022-07-31 01:16:48 +0100

    implemented add employee interface for the frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddEmployee from "./component/AddEmployee";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <AddEmployee />
+    </>
+  );
+}
+
+export default App;
